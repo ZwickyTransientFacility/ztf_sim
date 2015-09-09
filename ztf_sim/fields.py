add demo notebook; bugfix alt/az calculation

ztf_sim/fields.py
@@ -1,12 +1,32 @@
 """Routines for working with the ZTF discrete field grid"""
 
 import numpy as np
-import pandas
-from utils import df_write_to_sqlite, df_read_from_sqlite
+import pandas as pd
+from utils import *
+import astropy.coordinates as coords
+import astropy.units as u
+from astropy.time import Time
 
-def load_fields(dbname='test_fields'):
-    df = df_read_from_sqlite(dbname, index_col = 'fieldid')
-    return df
+class Fields:
+	"""Class for accessing field grid."""
+	# TODO: consider using some of PTFFields.py code 
+	def __init__(self,dbname='test_fields'):
+		self._load_fields(dbname=dbname)
+		self.loc = P48_loc
+		self.field_coords = coords.SkyCoord(self.fields['ra'], 
+			self.fields['dec'], frame='icrs', unit='deg')
+		# TODO: convert into Galactic & ecliptic coords and store
+		
+	def _load_fields(self, dbname='test_fields'):
+		df = df_read_from_sqlite(dbname, index_col = 'fieldid')
+		self.fields = df
+
+	def alt_az(self, time):
+		"""return Altitude & Azimuth by field at a given time"""
+		fieldsAltAz = self.field_coords.transform_to(coords.AltAz(obstime=time,
+			location=self.loc))
+		return pd.DataFrame({'alt':fieldsAltAz.alt, 'az':fieldsAltAz.az},
+			index = self.fields.index) 
 
 
 def generate_test_field_grid(dbname='test_fields'):
@@ -34,11 +54,10 @@ def generate_test_field_grid(dbname='test_fields'):
 
     fieldid = np.arange(len(thetas))
     ras = np.rad2deg(np.array(phis))
-     #equatorial Dec=0, positive at northern part
+    #equatorial Dec=0, positive at northern part
     decs = -(np.rad2deg(np.array(thetas))-90)
 
-    df = pandas.DataFrame({'ra':ras,'dec':decs},
+    df = pd.DataFrame({'ra':ras,'dec':decs},
         index = fieldid)
 
     df_write_to_sqlite(df,dbname, index_label='fieldid')
-

ztf_sim/utils.py
@@ -1,6 +1,6 @@
 
 import numpy as np
-import pandas
+import pandas as pd
 from astropy.time import Time
 import astropy.coordinates as coords
 import astropy.units as u
@@ -15,7 +15,7 @@ def df_write_to_sqlite(df, dbname, **kwargs):
 
 def df_read_from_sqlite(dbname, **kwargs):
     engine = create_engine('sqlite:///../data/{}.db'.format(dbname))
-    df = pandas.read_sql(dbname, engine, **kwargs)
+    df = pd.read_sql(dbname, engine, **kwargs)
 
     return df
 
@@ -26,14 +26,14 @@ def _ptf_to_sqlite():
 
     Fragile, as it assumes the formats below--not for general use."""
 
-    import pandas
+    import pd
 
     # main dump
     #e.expid, e.prid, f.ptffield, f.objrad, f.objdecd, e.fid, e.obsmjd,
     #e.nid, e.exptime, e.airmass,
     #e.obslst, e.altitude, e.azimuth, e.moonra, e.moondec, e.moonalt, e.moonphas, \
     #e.windspeed, e.outrelhu
-    df = pandas.read_table('../data/opsim_dump.txt.gz',sep='|',
+    df = pd.read_table('../data/opsim_dump.txt.gz',sep='|',
         names = ['obsHistID','propID','fieldID','fieldRA_deg','fieldDec_deg',
         'filter','expMJD','night','visitExpTime','airmass',
         'lst','altitude','azimuth','moonRA','moonDec','moonAlt','moonPhase',
@@ -42,7 +42,7 @@ def _ptf_to_sqlite():
 
     # sky
     #e.expid, qa.fwhmsex, sdqa.metricvalue
-    df_sky = pandas.read_table('../data/opsim_dump_sky.txt.gz',sep='|',
+    df_sky = pd.read_table('../data/opsim_dump_sky.txt.gz',sep='|',
         names = ['obsHistID','finSeeing', 'filtSkyBrightness'],
         # have to use converters otherwise filtSkyBrightness
         # becomes object and disappears in the mean; dtypes doesn't work 
@@ -58,7 +58,7 @@ def _ptf_to_sqlite():
 
     # limmag
     # e.expid, qa.fwhmsex, sdqa.metricvalue
-    df_lim = pandas.read_table('../data/opsim_dump_sky.txt.gz',sep='|',
+    df_lim = pd.read_table('../data/opsim_dump_sky.txt.gz',sep='|',
         names = ['obsHistID','finSeeing', 'fiveSigmaDepth'],
         converters = {'fiveSigmaDepth':lambda x: np.float(x)},
         skipfooter=1)