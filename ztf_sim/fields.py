d2043a7836b67387983df9fab5fc0a92ca9c9316
Eric Bellm <ebellm@caltech.edu>
2015-09-09 16:36:45 -0700

    calculate overhead time by field

--- ztf_sim/fields.py
+++ ztf_sim/fields.py
@@ -8,25 +8,56 @@ import astropy.units as u
 from astropy.time import Time
 
 class Fields:
-	"""Class for accessing field grid."""
-	# TODO: consider using some of PTFFields.py code 
-	def __init__(self,dbname='test_fields'):
-		self._load_fields(dbname=dbname)
-		self.loc = P48_loc
-		self.field_coords = coords.SkyCoord(self.fields['ra'], 
-			self.fields['dec'], frame='icrs', unit='deg')
-		# TODO: convert into Galactic & ecliptic coords and store
-		
-	def _load_fields(self, dbname='test_fields'):
-		df = df_read_from_sqlite(dbname, index_col = 'fieldid')
-		self.fields = df
+    """Class for accessing field grid."""
+    # TODO: consider using some of PTFFields.py code 
+    def __init__(self,dbname='test_fields'):
+        self._load_fields(dbname=dbname)
+        self.loc = P48_loc
+        # TODO: convert into Galactic & ecliptic coords and store
+        
+    def _load_fields(self, dbname='test_fields'):
+        """Loads a field grid from ../data/{dbname}.db.  
+        Expects fieldid, ra (deg), dec (deg) columns"""
+        df = df_read_from_sqlite(dbname, index_col = 'fieldid')
+        self.fields = df
 
-	def alt_az(self, time):
-		"""return Altitude & Azimuth by field at a given time"""
-		fieldsAltAz = self.field_coords.transform_to(coords.AltAz(obstime=time,
-			location=self.loc))
-		return pd.DataFrame({'alt':fieldsAltAz.alt, 'az':fieldsAltAz.az},
-			index = self.fields.index) 
+    def field_coords(self):
+        """Generate an astropy SkyCoord object for current fields"""
+        return coords.SkyCoord(self.fields['ra'], 
+            self.fields['dec'], frame='icrs', unit='deg')
+
+    def alt_az(self, time):
+        """return Altitude & Azimuth by field at a given time"""
+
+        fieldsAltAz = self.field_coords().transform_to(
+            coords.AltAz(obstime=time, location=self.loc))
+        return pd.DataFrame({'alt':fieldsAltAz.alt, 'az':fieldsAltAz.az},
+            index = self.fields.index) 
+
+    def overhead_time(self, current_fieldid, time, fieldids = None):
+        """Calculate overhead time in seconds from current position.
+        
+        fieldids is a subset of ids; if None, calculate for all ids"""
+        # TODO: figure out appropriate treatment of dome at zenith
+
+        df = self.fields.join(self.alt_az(time))
+
+        row = df.loc[current_fieldid]
+
+        slews_by_axis = {'readout':READOUT_TIME}
+        for axis in ['HA', 'Dec', 'Dome']:
+            coord = P48_slew_pars[axis]['coord']
+            dangle = np.abs(df[coord] - row[coord])
+            angle = np.where(dangle < (360. - dangle), dangle, 360. - dangle)
+            slews_by_axis[axis] = slew_time(axis, angle*u.deg)
+
+        dfslews = pd.DataFrame(slews_by_axis,index=df.index)
+
+        return dfslews.max(axis=1)
+            
+            
+
+        
 
 
 def generate_test_field_grid(dbname='test_fields'):
--- ztf_sim/utils.py
+++ ztf_sim/utils.py
@@ -9,6 +9,35 @@ from sqlalchemy import create_engine
 P48_loc = coords.EarthLocation(lat=coords.Latitude('33d21m26.35s'),
     lon=coords.Longitude('-116d51m32.04s'), height=1707.)
 
+P48_slew_pars = {
+    'HA':{'coord':'ra','accel':0.27*u.deg*u.second**(-2.),
+        'decel':0.27*u.deg*u.second**(-2.),'vmax':1.6*u.deg/u.second},
+    'Dec':{'coord':'dec','accel':0.2*u.deg*u.second**(-2.),
+        'decel':0.15*u.deg*u.second**(-2.),'vmax':1.2*u.deg/u.second},
+    'Dome':{'coord':'az','accel':0.17*u.deg*u.second**(-2.),
+        'decel':0.6*u.deg*u.second**(-2.),'vmax':3.*u.deg/u.second}}
+
+READOUT_TIME = 10.*u.second
+
+
+def slew_time(axis, angle):
+    vmax = P48_slew_pars[axis]['vmax']
+    acc = P48_slew_pars[axis]['accel']
+    dec = P48_slew_pars[axis]['decel']
+
+    t_acc=vmax/acc
+    t_dec=vmax/dec
+    # TODO: generalize to allow scalars
+    slew_time = 0.5*(2.*angle/vmax+t_acc+t_dec)
+    w = 0.5*vmax*(t_acc+t_dec) >= angle
+    slew_time[w] = np.sqrt(2*angle[w]*(1./acc+1./dec))
+#    if 0.5*vmax*(t_acc+t_dec)>=angle:
+#        return np.sqrt(2*angle*(1./acc+1./dec))
+#    else:
+#        return 0.5*(2.*angle/vmax+t_acc+t_dec)
+    return slew_time
+
+
 def df_write_to_sqlite(df, dbname, **kwargs):
     engine = create_engine('sqlite:///../data/{}.db'.format(dbname))
     df.to_sql(dbname, engine, if_exists='replace', **kwargs)
