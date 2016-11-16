e0ffe83c1e55e3c65129459f497726f3b0d6e901
Eric Bellm <ebellm@caltech.edu>
2016-11-16 10:55:05 -0800

    break observing sequence between nights

--- ztf_sim/observe.py
+++ ztf_sim/observe.py
@@ -70,6 +70,7 @@ def observe(run_name=run_name, start_time='2016-03-20 02:30:00',
 
         # check if it is a new night and reload queue with new requests
         if np.floor(tel.current_time.mjd) > current_night_mjd:
+            log.prev_obs = None
             Q.assign_nightly_requests(tel.current_state_dict())
             current_night_mjd = np.floor(tel.current_time.mjd)
 
