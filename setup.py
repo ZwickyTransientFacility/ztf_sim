9f632d2a5009c4bf08146570556e4cd745b7ec3d
Eric <ebellm@gmail.com>
2017-08-22 17:53:57 -0700

    add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 #
-# Copyright (C) 2015 California Institute of Technology
+# Copyright (C) 2015-17 California Institute of Technology
 
 DESCRIPTION = "ztf_sim: Schedule simulator for the Zwicky Transient Facility"
 LONG_DESCRIPTION = """\
@@ -9,11 +9,11 @@ Schedule simulator for the Zwicky Tranisent Facility
 
 DISTNAME = 'ztf_sim'
 MAINTAINER = 'Eric Bellm'
-MAINTAINER_EMAIL = 'ebellm@caltech.edu'
+MAINTAINER_EMAIL = 'ecbellm@uw.edu'
 URL = 'https://github.com/ZwickyTransientFacility/ztf_sim/'
 LICENSE = 'BSD (3-clause)'
 DOWNLOAD_URL = 'https://github.com/ZwickyTransientFacility/ztf_sim/'
-VERSION = '0.0.1.dev'
+VERSION = '0.0.2.dev'
 
 try:
     from setuptools import setup
@@ -21,34 +21,58 @@ try:
 except ImportError:
     from distutils.core import setup
 
-#def check_dependencies():
-#    install_requires = []
-#
-#    # Just make sure dependencies exist, I haven't rigorously
-#    # tested what the minimal versions that will work are
-#    # (help on that would be awesome)
-#    try:
-#        import numpy
-#    except ImportError:
-#        install_requires.append('numpy')
-#    try:
-#        import scipy
-#    except ImportError:
-#        install_requires.append('scipy')
-#    try:
-#        import matplotlib
-#    except ImportError:
-#        install_requires.append('matplotlib')
-#    try:
-#        import pandas
-#    except ImportError:
-#        install_requires.append('pandas')
-#
-#    return install_requires
+def check_dependencies():
+    install_requires = []
+
+    # Just make sure dependencies exist, I haven't rigorously
+    # tested what the minimal versions that will work are
+    # (help on that would be awesome)
+    try:
+        import numpy
+    except ImportError:
+        install_requires.append('numpy')
+    try:
+        import scipy
+    except ImportError:
+        install_requires.append('scipy')
+    try:
+        import astropy
+    except ImportError:
+        install_requires.append('astropy')
+    try:
+        import astroplan
+    except ImportError:
+        install_requires.append('astroplan')
+    try:
+        import pandas
+    except ImportError:
+        install_requires.append('pandas')
+    try:
+        import sklearn
+    except ImportError:
+        install_requires.append('sklearn')
+    try:
+        import sklearn_pandas
+    except ImportError:
+        install_requires.append('sklearn_pandas')
+    try:
+        import xgboost
+    except ImportError:
+        install_requires.append('xgboost')
+    try:
+        import transitions
+    except ImportError:
+        install_requires.append('transitions')
+    try:
+        import gurobipy
+    except ImportError:
+        install_requires.append('gurobipy')
+
+    return install_requires
 
 if __name__ == "__main__":
 
-#    install_requires = check_dependencies()
+    install_requires = check_dependencies()
 
     setup(name=DISTNAME,
         author=MAINTAINER,
