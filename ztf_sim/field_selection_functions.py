db8a20f19690148422915ce02b53e29791fa6d65
Eric Bellm <ecbellm@uw.edu>
2021-03-01 13:30:10 -0800

    implement priority selection for Caltech and Partnership

--- ztf_sim/field_selection_functions.py
+++ ztf_sim/field_selection_functions.py
@@ -125,6 +125,44 @@ def msip_nss_selection_phaseii(time, obs_log, other_program_fields, fields):
 
 def partnership_HC_selection(time, obs_log, other_program_fields, fields):
     """Select partnership HC fields"""
+    return phase_II_selection(time, obs_log, other_program_fields, fields,
+                              subprogram='Partnership')
+
+def Caltech_1DC_selection(time, obs_log, other_program_fields, fields):
+    """Select Caltech 1DC fields"""
+    return phase_II_selection(time, obs_log, other_program_fields, fields,
+                              subprogram='Caltech')
+
+
+def phase_II_selection(time, obs_log, other_program_fields, fields,
+        subprogram='Partnership', silent=False):
+    """Select partnership HC or Caltech 1DC fields"""
+
+    assert (subprogram in ['Partnership', 'Caltech'])
+
+    candidate_field_ids_prio1 = [473, 525, 624, 651, 671]
+    candidate_field_ids_prio2 = [349, 350, 406, 414, 479, 480, 481, 528, 529, 530, 531, 532, 533, 534, 544,
+545, 546, 576, 577, 578, 579, 580, 581, 582, 583, 584, 585, 596, 597, 600, 601,
+623, 625, 626, 627, 628, 629, 630, 631, 632, 633, 634, 635, 644, 645, 646, 647,
+668, 669, 670, 672, 673, 674, 675, 676, 677, 678, 679, 680, 681, 708, 709, 710,
+711, 712, 713, 714, 715, 716, 717, 718, 719, 720, 721, 722, 723, 724, 748, 749,
+750, 751, 752, 753, 754, 755, 756, 757, 758, 759, 760, 761, 762, 784, 785, 786,
+787, 788, 789, 790, 791, 792, 793, 794, 795, 796, 815, 816, 817, 818, 819, 820,
+821, 822, 823, 824, 825, 841, 843, 844]
+    candidate_field_ids_prio3 = [345, 346, 347, 348, 391, 392, 393, 394, 395, 396, 397, 398, 399, 400, 401,
+402, 415, 416, 417, 418, 419, 420, 421, 422, 423, 424, 425, 426, 427, 441, 442,
+443, 444, 445, 446, 447, 448, 449, 450, 451, 452, 464, 465, 466, 467, 468, 469,
+470, 471, 472, 474, 475, 476, 477, 478, 493, 494, 496, 497, 498, 499, 500, 501,
+502, 503, 516, 517, 518, 519, 520, 521, 522, 523, 524, 526, 527, 547, 548, 549,
+550, 551, 552, 553, 554, 566, 567, 568, 569, 570, 571, 572, 573, 574, 575, 599,
+602, 604, 615, 616, 617, 618, 619, 620, 621, 622, 662, 663, 664, 665, 666, 667,
+706, 707]
+    candidate_field_ids_prio4 = [296, 297, 298, 299, 300, 301, 302, 303, 304, 305, 306, 351, 352, 353, 354,
+355, 403, 405, 413, 482, 483, 484, 535, 543, 586, 594, 595, 682, 746, 747, 763,
+782, 783, 814, 839, 840, 842, 845, 846, 847, 848, 858, 859, 860, 861, 862, 863,
+864]
+
+    candidate_field_ids_byprio = [candidate_field_ids_prio1, candidate_field_ids_prio2, candidate_field_ids_prio3, candidate_field_ids_prio4]
 
     candidate_field_ids = [296, 297, 298, 299, 300, 301, 302, 303, 304, 305, 306, 345, 346, 347, 348, 349,
 350, 351, 352, 353, 354, 355, 391, 392, 393, 394, 395, 396, 397, 398, 399, 400,
@@ -144,27 +182,116 @@ def partnership_HC_selection(time, obs_log, other_program_fields, fields):
 763, 782, 783, 784, 785, 786, 787, 788, 789, 790, 791, 792, 793, 794, 795, 796,
 814, 815, 816, 817, 818, 819, 820, 821, 822, 823, 824, 825, 839, 840, 841, 842,
 843, 844, 845, 846, 847, 848, 858, 859, 860, 861, 862, 863, 864]
-    candidate_fields = fields.fields.loc[candidate_field_ids].copy()
 
-
-    HC_cadence = 1
-    HC_internight_gap = HC_cadence*u.day
-    HC_nobs_per_night = 4
-
-    HC_requests_allowed = other_program_fields[
+    # define the parameters that vary
+    if subprogram == 'Partnership':
+        label = 'Partnership HC'
+        min_hours_visible = 2.5
+        nobs_per_night = 4
+        requests_allowed = other_program_fields[
             (PROGRAM_NAME_TO_ID['collaboration'],'high_cadence')]['requests_allowed']
-    n_fields_to_observe = int(np.round(HC_requests_allowed / HC_nobs_per_night))
+        offset = 0
+        exclude_fields = []
 
-    # define a footprint for the 2-night cadence by cutting on HA at midnight
-    candidate_fields['HA_midnight'] = RA_to_HA(
-                candidate_fields['ra'].values*u.degree, 
-            P48_Observer.midnight(time, which='nearest')).wrap_at(180*u.deg)
-    candidate_fields['abs_HA_midnight'] = np.abs(candidate_fields['HA_midnight'])
-    HC_footprint_field_ids = candidate_fields.sort_values(by='abs_HA_midnight', ascending=True).iloc[:(HC_cadence*n_fields_to_observe-1)].index.tolist()
 
-    logger.info(f'Partnership HC: requesting {HC_footprint_field_ids}')
+    elif subprogram == 'Caltech':
+        label = 'Caltech 1DC'
+        min_hours_visible = 1.5
+        nobs_per_night = 2
+        requests_allowed = other_program_fields[
+            (PROGRAM_NAME_TO_ID['Caltech'],'Caltech_1DC')]['requests_allowed']
+        # to prevent fields from thrashing back and forth between programs
+        # we place a buffer in the priority list
+        offset = 15
+        exclude_fields = phase_II_selection(time, obs_log, other_program_fields, fields, subprogram='Partnership', silent=False)
 
-    return HC_footprint_field_ids
+
+    else:
+        raise ValueError(f'Unknown subprogram {subprogram}')
+
+
+    # do a visibility check (also handles moon exclusion)
+    visible_field_ids = fields.select_field_ids(dec_range=[-32,90.],
+                           grid_id=0,
+                           observable_hours_range=[min_hours_visible, 24.])
+
+    visible_field_ids = np.intersect1d(visible_field_ids, 
+            candidate_field_ids).tolist()
+
+    n_fields_to_observe = int(np.round(requests_allowed / nobs_per_night))
+    n_fields_remaining = n_fields_to_observe
+
+    if not silent:
+        logger.info(f'{label}: need {n_fields_to_observe} fields')
+
+    field_ids_to_observe = []
+
+    for i, prio_field_ids in enumerate(candidate_field_ids_byprio):
+
+        assert (n_fields_remaining >= 0)
+        if n_fields_remaining == 0:
+            break
+
+        visible_prio_field_ids = np.intersect1d(visible_field_ids,
+            prio_field_ids).tolist()
+
+        # for Caltech, exclude partnership fields being observed
+        visible_prio_field_ids = [f for f in visible_prio_field_ids if f not in exclude_fields]
+
+        if len(visible_prio_field_ids) == 0:
+            if not silent:
+                logger.info(f'{label}: No fields remaining at priority {i+1}')
+            continue
+
+        if len(visible_prio_field_ids) <= offset:
+            #always observe prio any 1 fields if available, without offset
+            # they weren't included in Partnership HC due to min_hours_visible
+            if i==0: # prio 1
+                field_ids_to_observe.extend(visible_prio_field_ids)
+                n_fields_remaining -= len(visible_prio_field_ids)
+                if not silent:
+                    logger.info(f'{label}: requesting {len(visible_prio_field_ids)} priority {i+1} fields: {visible_prio_field_ids}')
+
+            else:
+                offset -= len(visible_prio_field_ids)
+                if not silent:
+                    logger.info(f'{label}: Offsetting {len(visible_prio_field_ids)} at priority {i+1}')
+
+            continue
+
+        if (len(visible_prio_field_ids)+offset) <= n_fields_remaining:
+            # include all fields in this priority
+            field_ids_to_observe.extend(visible_prio_field_ids[offset:])
+            n_fields_remaining -= len(visible_prio_field_ids[offset:])
+            if not silent and offset != 0:
+                logger.info(f'{label}: Offsetting {offset} at priority {i+1}')
+            offset = 0
+            if not silent:
+                logger.info(f'{label}: requesting {len(visible_prio_field_ids[offset:])} priority {i+1} fields: {visible_prio_field_ids}')
+        else:
+            # prioritize by cutting on HA at midnight
+            
+            if not silent and offset != 0:
+                logger.info(f'{label}: Offsetting {offset} at priority {i+1}')
+
+            candidate_fields = fields.fields.loc[visible_prio_field_ids].copy()
+
+            candidate_fields['HA_midnight'] = RA_to_HA(
+                        candidate_fields['ra'].values*u.degree, 
+                    P48_Observer.midnight(time, which='nearest')).wrap_at(180*u.deg)
+            candidate_fields['abs_HA_midnight'] = np.abs(candidate_fields['HA_midnight'])
+            footprint_field_ids = candidate_fields.sort_values(
+                    by='abs_HA_midnight', ascending=True).iloc[offset:offset+n_fields_remaining].index.tolist()
+            offset=0
+            n_fields_remaining -= len(footprint_field_ids)
+            field_ids_to_observe.extend(footprint_field_ids)
+            if not silent:
+                logger.info(f'{label}: requesting {len(footprint_field_ids)} priority {i+1} fields of {len(candidate_fields)} total: {footprint_field_ids}')
+
+    if not silent:
+        logger.info(f'{label}: requesting {field_ids_to_observe}')
+
+    return field_ids_to_observe
 
 def msip_nss_selection(time, obs_log, other_program_fields, fields):
     """Select MSIP NSS fields so they don't overlap with other MSIP subprograms."""
