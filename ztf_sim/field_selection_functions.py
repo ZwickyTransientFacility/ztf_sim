6ac164dd3ee172f832b09f105db4f79e9d091a45
Eric Bellm <ecbellm@uw.edu>
2024-11-21 15:19:23 -0800

    bugfix

--- ztf_sim/field_selection_functions.py
+++ ztf_sim/field_selection_functions.py
@@ -114,7 +114,7 @@ def msip_o4_skymap_selection(time, obs_log, other_program_fields, fields,
         #TODO: need to deduplicate any overlaps
 
     # ComCam through Dec. 15, '24
-    field_ids_to_observe.extend(252, 1246, 452, 258)      
+    field_ids_to_observe.extend([252, 1246, 452, 258])      
     field_ids_to_observe = list(set(field_ids_to_observe))
     
     # if there is any time left, fill with regular NSS selection
